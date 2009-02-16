f7cd61a866763002e76c2e1a44794fe2a74396e7
Asa Hopkins <open.campaigns@gmail.com>
2009-02-16 14:01:19 -0800

    fixes to railspdf plugin

--- vendor/plugins/railspdf/lib/railspdf.rb
+++ vendor/plugins/railspdf/lib/railspdf.rb
@@ -4,32 +4,43 @@ require 'pdf/simpletable'
 module RailsPDF
   
  	#this code comes from http://wiki.rubyonrails.com/rails/pages/HowtoGeneratePDFs 	
-	class PDFRender < ActionView::Base
+	class PDFRender < ActionView::TemplateHandler
   	include ApplicationHelper
+    # include ActionView::Helpers::TranslationHelper
+    # include ActionView::Helpers::AssetTagHelper
+    # include ActionView::Helpers::TextHelper
+    # include ActionView::Helpers::TagHelper
+    # include ActionView::Helpers::UrlHelper
 
   	def initialize(action_view)
     		@action_view = action_view
-  			prefix = action_view.controller.class.to_s.gsub(/Controller/, '')
-        self.class.send(:include, "#{prefix}Helper".constantize)
+  			helper_name = "#{action_view.controller.controller_name.classify}Helper"
+        self.class.send(:include, helper_name.constantize) if Object.const_defined?(helper_name)
   	end
+  	
+  	def self.compilable?
+      false
+    end
 
-  
+    def compilable?
+      self.class.compilable?
+    end
+
     def render(template, local_assigns = {})
     	
     	#get the instance variables setup	    	
    		@action_view.controller.instance_variables.each do |v|
-       		instance_variable_set(v, @action_view.controller.instance_variable_get(v))
+        instance_variable_set(v, @action_view.controller.instance_variable_get(v))
       end
 		
 			#keep ie happy
 			if @action_view.controller.request.env['HTTP_USER_AGENT'] =~ /msie/i
-        		@action_view.controller.headers['Pragma'] ||= ''
-        		@action_view.controller.headers['Cache-Control'] ||= ''
+        @action_view.controller.headers['Pragma'] ||= ''
+        @action_view.controller.headers['Cache-Control'] ||= ''
    		else
-        		@action_view.controller.headers['Pragma'] ||= 'no-cache'
-        		@action_view.controller.headers['Cache-Control'] ||= 'no-cache, must-revalidate'
+        @action_view.controller.headers['Pragma'] ||= 'no-cache'
+        @action_view.controller.headers['Cache-Control'] ||= 'no-cache, must-revalidate'
    		end
-   		
    		
    		@action_view.controller.headers["Content-Type"] ||= 'application/pdf'
 			if @rails_pdf_name
@@ -48,12 +59,12 @@ module RailsPDF
       end
  	  	pdf.compressed = true if RAILS_ENV != 'development'
  	  	
-	    eval template.source, nil, "#{@action_view.base_path}/#{@action_view.first_render}.#{@action_view.finder.pick_template_extension(@action_view.first_render)}" 
+	    eval template.source, nil
    		pdf.render
   	end
   	
-  	def compilable?
-      false
+  	def self.call(template)
+      "RailsPDF::PDFRender.new(self).render(template, local_assigns)"
     end
   end
 end
